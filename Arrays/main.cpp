189a37758d4ab8a874934dd1a6e787bb4b1c8108
Артём Межинский <aswert092@gmail.com>
2022-05-07 15:35:47 +0300

    Templation in progress

---
+++ Arrays/main.cpp
@@ -0,0 +1,157 @@
+#include<iostream>
+using namespace std;
+
+void FillRand(int arr[], const int n);
+void FillRand(double arr[], const int n);
+
+template<typename T>
+void Print(T arr[], const int n);
+template<typename T>
+void Sort(T arr[], const int n);
+
+template<typename T>
+T Sum(T arr[], const int n);
+
+double Avg(int arr[], const int n);	//Avg (Avarage - Среднее арифметическое)
+double Avg(double arr[], const int n);	//Avg (Avarage - Среднее арифметическое)
+
+int minValueIn(int arr[], const int n);
+double minValueIn(double arr[], const int n);
+
+int maxValueIn(int arr[], const int n);
+double maxValueIn(double arr[], const int n);
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	const int n = 5;
+	int arr[n];
+
+	cout << ("Hello", "World") << endl;//Comma operator (оператор запятая) - позволяет поместить несколько выражений, 
+	//в то место, где ожидается одно выражение. Выражения должны быть одного типа.
+	//Оператор запятая возвращает значение последнего выражения (ассоциативность слева направо).
+
+	FillRand(arr, n);
+	Print(arr, n);
+	cout << "Сумма элементов массива: " << Sum(arr, n) << endl;
+	cout << "Среднее-арифметическое элементов массива: " << Avg(arr, n) << endl;
+	cout << "Минимальное значение в массиве: " << minValueIn(arr, n) << endl;
+	cout << "Максимальное значение в массиве: " << maxValueIn(arr, n) << endl;
+	Sort(arr, n);
+	Print(arr, n);
+
+	////////////////////////////////////////////////////////////////
+
+	const int m = 8;
+	double brr[m];
+	FillRand(brr, m);
+	Print(brr, m);
+	cout << "Сумма элементов массива: " << Sum(brr, m) << endl;
+	cout << "Среднее-арифметическое элементов массива: " << Avg(brr, m) << endl;
+	cout << "Минимальное значение в массиве: " << minValueIn(brr, m) << endl;
+	cout << "Максимальное значение в массиве: " << maxValueIn(brr, m) << endl;
+	Sort(brr, m);
+	Print(brr, m);
+}
+void FillRand(int arr[], const int n)
+{
+	//Заполняем случайными чослами:
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = rand() % 100;
+	}
+}
+void FillRand(double arr[], const int n)
+{
+	//Заполняем случайными чослами:
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = rand() % 10000;
+		arr[i] /= 100;
+	}
+}
+
+template<typename T>
+void Print(T arr[], const int n)
+{
+	//Вывод массива на экран:
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << "\t";
+	}
+	cout << endl;
+}
+template<typename T>
+void Sort(T arr[], const int n)
+{
+	//Сортировка массива:
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = i + 1; j < n; j++)
+		{
+			if (arr[j] < arr[i])
+			{
+				T buffer = arr[i];
+				arr[i] = arr[j];
+				arr[j] = buffer;
+			}
+		}
+	}
+}
+template<typename T>
+T Sum(T arr[], const int n)
+{
+	T sum = 0;
+	for (int i = 0; i < n; i++)
+	{
+		sum += arr[i];
+	}
+	return sum;
+}
+
+double Avg(int arr[], const int n)
+{
+	return (double)Sum(arr, n) / n;
+}
+double Avg(double arr[], const int n)
+{
+	return (double)Sum(arr, n) / n;
+}
+
+int minValueIn(int arr[], const int n)
+{
+	int min = arr[0];
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] < min)min = arr[i];
+	}
+	return min;
+}
+double minValueIn(double arr[], const int n)
+{
+	double min = arr[0];
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] < min)min = arr[i];
+	}
+	return min;
+}
+
+int maxValueIn(int arr[], const int n)
+{
+	int max = arr[0];
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] > max)max = arr[i];
+	}
+	return max;
+}
+double maxValueIn(double arr[], const int n)
+{
+	double max = arr[0];
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] > max)max = arr[i];
+	}
+	return max;
+}
